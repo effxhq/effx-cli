239d4d2d7ce0b1dab6d73c32c0bdd910cc33f292
Eric Uriostigue <eric@effx.com>
2020-12-09 11:34:23 -0500

    Onboarding Setup Script (#36)

---
+++ s3/dev_setup.sh
@@ -0,0 +1,99 @@
+curl -i -X PUT "http://localhost:4000/v2/services" -H "accept: application/json" -H "X-Effx-Api-Key: $EFFX_API_KEY" -H "Content-Type: application/json" -d'
+{
+  "version": "effx/v1",
+  "kind": "service",
+  "spec": {
+    "name": "example-service",
+    "description": "this service contains useful links for using Effx",
+    "tags": {
+      "go": "1.15.4",
+      "group": "example"
+    },
+    "annotations": {
+      "effx.io/owned-by": "example team"
+    },
+    "contact": {
+      "email": "support@effx.com",
+      "chat": {
+        "label": "#community",
+        "url": "https://join.slack.com/t/m11s/shared_invite/zt-j71p8afk-07MmPgrXlUd7qF_s6qXovg"
+      }
+    },
+    "linkGroups": [
+      {
+        "label": "documentation",
+        "links": [
+          {
+            "url": "http://help.effx.com",
+            "label": "help"
+          },
+          {
+            "url": "https://effxhq.github.io/effx-api-v2/",
+            "label": "API"
+          },
+          {
+            "url": "https://github.com/effxhq/vcs-connect/blob/main/docs/gitlab.md",
+            "label": "vcs-connect: gitlab"
+          },
+          {
+            "url": "https://github.com/effxhq/vcs-connect/blob/main/docs/github.md",
+            "label": "vcs-connect: github"
+          }
+        ]
+      },
+      {
+        "label": "dashboards",
+        "links": [
+          {
+            "url": "http://status.effx.com",
+            "label": "status"
+          },
+          {
+            "url": "https://p.datadoghq.com/sb/f655e3381-6bdce95d9ec6f133df05532b21b5f5dc",
+            "label": "datadog"
+          }
+        ]
+      },
+      {
+        "label": "runbook",
+        "links": [
+          {
+            "url": "https://blog.effx.com/how-to-write-a-runbook/",
+            "label": "best practices"
+          }
+        ]
+      },
+      {
+        "label": "version control",
+        "links": [
+          {
+            "url": "https://github.com/effxhq/.github",
+            "label": "github workflows"
+          },
+          {
+            "url": "https://github.com/effxhq/vcs-connect",
+            "label": "vcs-connect"
+          }
+        ]
+      },
+      {
+        "label": "continuous integration",
+        "links": [
+          {
+            "url": "https://github.com/effxhq/.github/blob/main/workflow-templates/effx-cli-ci.yml",
+            "label": "github workflows"
+          },
+          {
+            "url": "https://github.com/marketplace?type=actions&query=effxhq",
+            "label": "github actions"
+          },
+          {
+            "url": "https://circleci.com/developer/orbs/orb/effx/effx-cli",
+            "label": "circleci Orb"
+          }
+        ]
+      }
+    ]
+  }
+}
+'
