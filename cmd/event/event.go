a62287cf680078bbd1141714d740072494656bbd
Daniel McSheehy <dsm140130@utdallas.edu>
2021-01-29 13:12:30 -0800

    adding shorthand

--- cmd/event/event.go
+++ cmd/event/event.go
@@ -17,12 +17,12 @@ var (
 
 func init() {
 	EventCmd.PersistentFlags().StringVarP(&apiKeyString, "key", "k", "", "your effx api key. alternatively, you can use env var EFFX_API_KEY")
-	EventCmd.PersistentFlags().StringVarP(&result.Title, "title", "", "", "name of the event")
-	EventCmd.PersistentFlags().StringVarP(&result.Message, "message", "", "", "message to describe the event")
-	EventCmd.PersistentFlags().StringVarP(&result.ServiceName, "service", "", "", "service name the event is associated with")
-	EventCmd.PersistentFlags().StringVarP(&result.Tags, "tags", "", "", "tags in the format of k:v,k1:v1 . use commas to separatewe tags")
-	EventCmd.PersistentFlags().StringVarP(&result.Actions, "actions", "", "", "actions in the format of <level>:<name>:<url>")
-	EventCmd.PersistentFlags().IntVarP(&result.ProducedAtTimeMS, "produced_at_time", "", 0, "optional time the event was created at. format is epoch milliseconds. default is current time")
+	EventCmd.PersistentFlags().StringVarP(&result.Title, "title", "tt", "", "name of the event")
+	EventCmd.PersistentFlags().StringVarP(&result.Message, "message", "msg", "", "message to describe the event")
+	EventCmd.PersistentFlags().StringVarP(&result.ServiceName, "service", "sn", "", "service name the event is associated with")
+	EventCmd.PersistentFlags().StringVarP(&result.Tags, "tags", "", "tg", "tags in the format of k:v,k1:v1 . use commas to separatewe tags")
+	EventCmd.PersistentFlags().StringVarP(&result.Actions, "actions", "act", "", "actions in the format of <level>:<name>:<url>")
+	EventCmd.PersistentFlags().IntVarP(&result.ProducedAtTimeMS, "produced_at_time", "timestamp", 0, "optional time the event was created at. format is epoch milliseconds. default is current time")
 }
 
 var EventCmd = &cobra.Command{
