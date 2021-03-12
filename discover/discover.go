bbe9ae6ba7837fa13d317cd42fdc525df63b4f63
Daniel McSheehy <dsm140130@utdallas.edu>
2021-03-12 09:00:56 -0800

    gotestsum

--- discover/discover.go
+++ discover/discover.go
@@ -145,3 +145,49 @@ func SendDetectedServices(apiKey string, url *url.URL, servicePayloads []effx_ap
 	log.Println("Successfully detected ", len(servicePayloads), " service(s)")
 	return nil
 }
+
+// DetectServices (DEPRECATED) is an endpoint to detect services
+func DetectServices(sourceName string, effxFileLocations []string) []effx_api.DetectedServicesPayload {
+	detectedServices := []effx_api.DetectedServicesPayload{}
+
+	commonDir := findCommonDirectory(effxFileLocations)
+	files, err := ioutil.ReadDir(commonDir)
+	if err != nil {
+		return detectedServices
+	}
+	for _, file := range files {
+		// looking at directories only for service locations
+		if file.IsDir() {
+
+			contains := directoryContainsEffxYaml(file, effxFileLocations)
+			if !contains {
+				payload := effx_api.DetectedServicesPayload{
+					Name:       strings.ToLower(file.Name()),
+					SourceName: &sourceName,
+					Tags:       &map[string]string{},
+				}
+				if os.Getenv("DISABLE_LANGUAGE_DETECTION") != "true" {
+					result, err := metadata.InferMetadata(filepath.Dir(commonDir + file.Name()))
+					if err != nil {
+						log.Printf("Could not predict version %+v\n", err)
+					}
+
+					if result != nil {
+						if result.Language != "" {
+							language := strings.ToLower(result.Language)
+							(*payload.Tags)["language"] = language
+
+							// version cannot be found if language cannot be detected
+							if result.Version != "" {
+								(*payload.Tags)[language] = strings.ToLower(result.Version)
+							}
+						}
+					}
+				}
+
+				detectedServices = append(detectedServices, payload)
+			}
+		}
+	}
+	return detectedServices
+}
