27d1433187caaa6cbc9e003f2206e2d1d75e6e44
Daniel McSheehy <dsm140130@utdallas.edu>
2021-03-15 09:39:39 -0700

    test file

--- discover/discover_test.go
+++ discover/discover_test.go
@@ -10,6 +10,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_Discover_Services(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "fakedir")
+	defer os.RemoveAll(dir)
+
+	_, _ = os.Create(dir + "/package.json")
+
+	res, err := discover.DetectServicesFromFiles(dir, []data.EffxYaml{}, "effx-cli")
+
+	require.Nil(t, err)
+	require.Len(t, res, 1)
+	require.Contains(t, res[0].Name, "fakedir")
+}
+
+func Test_Nested_DirectoryName(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "apps")
+	defer os.RemoveAll(dir)
+
+	_, _ = ioutil.TempDir(dir, "dooku")
+
+	res, err := discover.DetectServicesFromFiles(dir, []data.EffxYaml{}, "effx-cli")
+
+	require.Nil(t, err)
+	require.Len(t, res, 1)
+	require.Contains(t, res[0].Name, "dooku")
+}
+
 func Test_Discover_Services_From_Yaml(t *testing.T) {
 	dir, _ := ioutil.TempDir("", "services")
 	defer os.RemoveAll(dir)
@@ -36,30 +62,4 @@ func Test_Discover_Services_From_Yaml(t *testing.T) {
 
 	require.Len(t, res, 1)
 	require.Contains(t, res[0].Name, "watto")
-}
-
-func Test_Discover_Services(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "fakedir")
-	defer os.RemoveAll(dir)
-
-	_, _ = os.Create(dir + "/package.json")
-
-	res, err := discover.DetectServicesFromFiles(dir, []data.EffxYaml{}, "effx-cli")
-
-	require.Nil(t, err)
-	require.Len(t, res, 1)
-	require.Contains(t, res[0].Name, "fakedir")
-}
-
-func Test_Nested_DirectoryName(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "apps")
-	defer os.RemoveAll(dir)
-
-	_, _ = ioutil.TempDir(dir, "dooku")
-
-	res, err := discover.DetectServicesFromFiles(dir, []data.EffxYaml{}, "effx-cli")
-
-	require.Nil(t, err)
-	require.Len(t, res, 1)
-	require.Contains(t, res[0].Name, "dooku")
 }
